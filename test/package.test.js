0c63e182d9c298cb1ced4d8d8cb68662cf23fb8f
Andrew Lavers <alavers@zendesk.com>
2025-12-10 15:18:52 -0500

    Update dependencies (#8)
    * Update dev deps and fix unit tests
    
    * Update dependencies
    
    * Add test github action
    
    * Bump version

--- test/package.test.js
+++ test/package.test.js
@@ -1,16 +1,13 @@
-/* global describe, it, expect */
+const { expect } = require('chai');
+const strategy = require('..');
 
-var strategy = require('..');
-
-describe('passport-zendesk', function() {
-    
-  it('should export Strategy constructor directly from package', function() {
+describe('passport-zendesk', function () {
+  it('should export Strategy constructor directly from package', function () {
     expect(strategy).to.be.a('function');
     expect(strategy).to.equal(strategy.Strategy);
   });
-  
-  it('should export Strategy constructor', function() {
+
+  it('should export Strategy constructor', function () {
     expect(strategy.Strategy).to.be.a('function');
   });
-  
 });
